41453b2f84eb5823f8c1fa54e31e32cc53185291
Roman <petrenkorv95@gmail.com>
2023-12-10 00:56:05 +0700

    first commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,329 @@
+jQuery(function ($) {
+    $("#burger").on("click", function (e) {
+        e.preventDefault();
+        $("body").toggleClass("mobile-menu");
+    });
+    $(".mobileMenu__link-sub").on("click", function (e) {
+        $(this).toggleClass("active");
+    });
+    const mainsliderSmall = new Swiper(".mainslider-swiper-small", {
+        speed: 700,
+        allowTouchMove: false,
+        loop: true,
+    });
+    const mainsliderText = new Swiper(".mainslider-text", {
+        speed: 700,
+        allowTouchMove: false,
+        effect: "fade",
+        fadeEffect: {
+            crossFade: true, 
+        }, 
+    });
+    const mainslider = new Swiper(".mainslider-swiper", {
+        speed: 700,
+        autoplay: {
+            delay: 2000,
+        },
+        loop: true,
+        pagination: {
+            el: "#mainslider-page",
+            clickable: true,
+        },
+        thumbs: {
+            swiper: mainsliderText,
+        },
+    });
+    mainslider.controller.control = mainsliderSmall;
+    mainsliderSmall.controller.control = mainslider;
+
+    const maincards = new Swiper(".maincards__grid2", {
+        slidesPerView: 1.5,
+        spaceBetween: 16,
+        breakpoints: {
+            768: {
+                slidesPerView: 2.5,
+            },
+            1024: {
+                slidesPerView: 4,
+            },
+        },
+    });
+    const mainpricing = new Swiper(".mainpricing__cards", {
+        slidesPerView: 1.5,
+        spaceBetween: 16,
+        breakpoints: {
+            768: {
+                slidesPerView: 3.5,
+            },
+            1024: {
+                slidesPerView: 6,
+            },
+        },
+    });
+
+    const faq = new Swiper(".sfaq__tabs", {
+        allowTouchMove: false,
+    });
+    $(".sfaq__links").on('click', 'a', function(e){
+        e.preventDefault();
+        $(".sfaq__links .active").removeClass('active');
+        $(this).addClass('active');
+        faq.slideTo($(this).data('tab'));
+    });
+   
+
+    $(".maincard__title").hover(
+        function () {
+            $(this).closest(".maincard__body").addClass("hover");
+        },
+        function () {
+            $(this).closest(".maincard__body").removeClass("hover");
+        }
+    );
+
+    const maincardBtnAnim = gsap.timeline({ paused: true });
+    maincardBtnAnim
+        .to("#maincards-cart", { duration: 0.2, x: 30 })
+        .to("#maincards-cart", { duration: 0.2, x: -30 })
+        .to("#maincards-cart", { duration: 0.2, x: 10 })
+        .to("#maincards-cart", { duration: 0.2, x: -10 })
+        .to("#maincards-cart", { duration: 0.2, x: 0 });
+    
+    $(".front-page .maincard input").on("change", function (e) {
+        let c = $("#maincards-cart .count"),
+            count = $(".maincard input:checked").length;
+        c.html(count);
+        maincardBtnAnim.restart();
+        if (count > 0) {
+            c.show();
+            $("#maincards-cart .text").text("Continue");
+        } else {
+            c.hide();
+            $("#maincards-cart .text").text("Sign up now");
+        }
+        if (!$(this).prop("checked")) {
+            $(this).parent().removeClass("active");
+            return;
+        }
+        $(this)
+            .parent()
+            .addClass("active")
+            .clone()
+            .css({ position: "absolute", "z-index": "100", bottom: "0" })
+            .appendTo($(this).parent())
+            .animate(
+                {
+                    bottom: -150,
+                    opacity: 0,
+                },
+                700,
+                function () {
+                    $(this).remove();
+                }
+            );
+    });
+
+    const pricingBtnAnim = gsap.timeline({ paused: true });
+    pricingBtnAnim
+        .to("#mainpricing-cart", { duration: 0.2, x: 30 })
+        .to("#mainpricing-cart", { duration: 0.2, x: -30 })
+        .to("#mainpricing-cart", { duration: 0.2, x: 10 })
+        .to("#mainpricing-cart", { duration: 0.2, x: -10 })
+        .to("#mainpricing-cart", { duration: 0.2, x: 0 });
+    $(".front-page .pricingcard input").on("change", function (e) {
+        let c = $("#mainpricing-cart .count"),
+            count = $(".pricingcard input:checked").length;
+        c.html(count);
+        pricingBtnAnim.restart();
+        if (count > 0) {
+            c.show();
+            $("#mainpricing-cart .text").text("Continue");
+        } else {
+            c.hide();
+            $("#mainpricing-cart .text").text("Let's go");
+        }
+        if (!$(this).prop("checked")) {
+            $(this).parent().removeClass("active");
+            return;
+        }
+        $(this)
+            .parent()
+            .addClass("active")
+            .clone()
+            .css({ position: "absolute", "z-index": "100", bottom: "0" })
+            .appendTo($(this).parent())
+            .animate(
+                {
+                    bottom: -150,
+                    opacity: 0,
+                },
+                700,
+                function () {
+                    $(this).remove();
+                }
+            );
+    });
+
+    $(".pricing-page .pricingcard input").on("change", function (e) {
+        let c = $(".pricingCards"),
+            count = $(".pricingcard input:checked").length;
+        if (count > 0) {
+            c.slideDown();
+        } else {
+            c.slideUp();
+        }
+        $(".pricingcard input").each(function(){
+            if(!$(this).prop('checked')){
+                $(this).closest('.pricingcard').removeClass('active');
+            }
+        });
+        // if (!$(this).prop("checked")) {
+        //     $(this).parent().removeClass("active");
+        //     return;
+        // }
+        $(this)
+            .parent()
+            .addClass("active");
+            let title = $(this).parent().find(".pricingcard__title").text();
+            $(".pricingCards__title").text(title);
+            $([document.documentElement, document.body]).animate(
+                {
+                    scrollTop: $(".pricingCards__title").offset().top,
+                },
+                500
+            );
+    });
+
+    $(".pricingCards__item_btn").hover(
+        function () {
+            $(this).closest(".pricingCards__item").addClass("hover");
+        },
+        function () {
+            $(this).closest(".pricingCards__item").removeClass("hover");
+        }
+    );
+
+
+    // checkoutHeader__items;
+    // checkout__tab;
+
+
+
+    function isInView(elem) {
+        return (
+            elem.offset().top - $(window).height() / 2 <
+                $(window).scrollTop() &&
+            elem.offset().top + elem.outerHeight() - $(window).height() / 2 >
+                $(window).scrollTop()
+        );
+    }
+
+    function isInViewStep(elem) {
+        let centerScreen = $(window).scrollTop() + $(window).height() / 2;
+        return (
+            elem.offset().top < centerScreen &&
+            elem.offset().top + elem.outerHeight(true) > centerScreen
+        );
+    }
+    $(window).scroll(function () {
+        if ($(".maininfo").length) {
+            if (isInView($(".maininfo__items"))) {
+                let indexStep = 0;
+                $(".maininfo__item").each(function (index, element) {
+                    if (isInViewStep($(this))) {
+                        indexStep = index;
+                    }
+                });
+
+                if (
+                    !$(".maininfo__counter li").eq(indexStep).hasClass("active")
+                ) {
+                    $(".maininfo__counter li.active").removeClass("active");
+                    $(".maininfo__counter li").eq(indexStep).addClass("active");
+                }
+            }
+        }
+    });
+
+    const onScrollHeader = () => {
+        const header = $(".header");
+        let prevScroll = $(window).scrollTop();
+        let currentScroll;
+        $(window).scroll(() => {
+            currentScroll = $(window).scrollTop();
+            const headerHidden = () => header.hasClass("header_hidden");
+            if (currentScroll > prevScroll && !headerHidden()) {
+                header.addClass("header_hidden");
+            }
+            if (currentScroll < prevScroll && headerHidden()) {
+                header.removeClass("header_hidden");
+            }
+            prevScroll = currentScroll;
+        });
+    };
+    onScrollHeader();
+
+    $(".faqItem__header").on('click', function(e){
+        $(this).toggleClass("active").closest(".faqItem").find(".faqItem__body").slideToggle();
+    });
+
+    $(".pricing-page .selectbox").chosen({width:180});
+
+    // const teamsize = new CustomSelect("#teamsize");
+    // const adminsize = new CustomSelect("#adminsize");
+    
+  
+    // GSAP
+    gsap.to(".mainparallax__wrapper", {
+        scrollTrigger: {
+            trigger: ".mainparallax",
+            scrub: 2,
+            start: "top center",
+            end: "+=200",
+            ease: "power1.out",
+        },
+        scale: 1,
+    });
+    gsap.to(".mainhead__title_row-1", {
+        scrollTrigger: {
+            trigger: ".mainhead",
+            scrub: 2,
+            start: "top",
+            ease: "power1.out",
+        },
+        y: -50,
+    });
+    gsap.to(".mainhead__title_row-2", {
+        scrollTrigger: {
+            trigger: ".mainhead",
+            scrub: 2,
+            start: "top",
+            ease: "power1.out",
+        },
+        y: -230,
+    });
+    gsap.to('.mainhead__text', {
+        backgroundPositionY: "100%",
+        duration:1
+    })
+
+    const growTl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".mainparallax",
+            scrub: 1.5,
+            start: "top center",
+            end: "-=200",
+            ease: "power1.out",
+        },
+    });
+    growTl.to(".mainparallax__logo", {
+        duration: 1,
+        opacity: 1,
+        top: "45%",
+    });
+    growTl.to(".mainparallax__logo", {
+        duration: 1,
+        delay: -0.1,
+        width: "auto",
+    });
+});
